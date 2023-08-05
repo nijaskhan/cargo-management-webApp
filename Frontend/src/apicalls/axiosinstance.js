af13f30fadeb92786ee86f6ce0e203eb96608411
nijaskhan <nijaskhan93@gmail.com>
2023-08-05 18:26:04 +0530

    changed conf to production

--- Frontend/src/apicalls/axiosinstance.js
+++ Frontend/src/apicalls/axiosinstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: "http://localhost:3000",
+  // change when adding to production server
+    // baseURL: "http://localhost:3000",   //dev sever
+    baseURL: "https://cargo-management-service.onrender.com",    //demo production server
   });
 
 
